97c3f48b9e5aa5cfabe7ec67ecd7c9f0ea25018e
jiguiquan <jiguiquan@163.com>
2020-03-08 19:24:22 +0800

    新增cloud-eureka-server7001服务（单机版）

---
+++ cloud-eureka-server7001/src/main/java/com/jiguiquan/springcloud/EurekaMain7001.java
@@ -0,0 +1,17 @@
+package com.jiguiquan.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author jiguiquan
+ * @create 2020-03-08 18:30
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}

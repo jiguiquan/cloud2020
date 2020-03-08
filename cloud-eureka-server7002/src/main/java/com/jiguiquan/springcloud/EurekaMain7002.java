b3b43774fe215a91e87ac8c7574f3d0d150600be
jiguiquan <jiguiquan@163.com>
2020-03-08 20:37:56 +0800

    新建cloud-eureka-server7002模块，搭建EurekaServer集群

---
+++ cloud-eureka-server7002/src/main/java/com/jiguiquan/springcloud/EurekaMain7002.java
@@ -0,0 +1,17 @@
+package com.jiguiquan.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author jiguiquan
+ * @create 2020-03-08 19:44
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}

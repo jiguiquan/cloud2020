df13bd2d09b2772460579315ecad062c5cdbe3c5
jiguiquan <jiguiquan@163.com>
2020-03-08 19:25:22 +0800

    cloud-provider-payment8001和cloud-consumer-prder80服务分别入驻EurekaServer

--- cloud-consumer-order80/src/main/java/com/jiguiquan/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/jiguiquan/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.jiguiquan.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author jiguiquan
  * @create 2020-03-07 13:55
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
--- cloud-provider-payment8001/src/main/java/com/jiguiquan/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/jiguiquan/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.jiguiquan.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author jiguiquan
  * @create 2020-03-07 10:32
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);

b9bba717f8425ba33b847aecdbed154351ff575c
jiguiquan <jiguiquan@163.com>
2020-03-29 16:31:24 +0800

    新建config-center3344服务，config统一配置中心

---
+++ cloud-config-center3344/src/main/java/com/jiguiquan/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.jiguiquan.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author jiguiquan
+ * @create 2020-03-29 15:21
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}

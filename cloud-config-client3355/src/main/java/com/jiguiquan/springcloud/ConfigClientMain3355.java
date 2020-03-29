b9d058b8cbda45c0b51b48208640dab350430198
jiguiquan <jiguiquan@163.com>
2020-03-29 22:31:28 +0800

    新增config-client模块

---
+++ cloud-config-client3355/src/main/java/com/jiguiquan/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,17 @@
+package com.jiguiquan.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author jiguiquan
+ * @create 2020-03-29 16:48
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
---
+++ cloud-config-client3355/src/main/java/com/jiguiquan/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.jiguiquan.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jiguiquan
+ * @create 2020-03-29 16:51
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}

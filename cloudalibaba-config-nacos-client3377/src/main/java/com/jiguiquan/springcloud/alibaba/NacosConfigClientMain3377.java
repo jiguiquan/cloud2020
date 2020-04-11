7a615cf1aea6fb2363c38f2e91ea1f0c1a7b7c68
jiguiquan <jiguiquan@163.com>
2020-04-11 18:04:33 +0800

    增加服务3377，测试使用Nacos作为服务配置中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/jiguiquan/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,17 @@
+package com.jiguiquan.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author jiguiquan
+ * @create 2020-04-11 16:40
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/jiguiquan/springcloud/alibaba/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.jiguiquan.springcloud.alibaba.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jiguiquan
+ * @create 2020-04-11 16:42
+ */
+@RestController
+@RefreshScope  //为了自动动态刷新
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping(value = "/config/info")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
